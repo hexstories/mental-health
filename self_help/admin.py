c5ae850ae2cf8d23b822c4383ff5e320e1f42f28
hexstories <kwakuasihene@gmail.com>
2024-06-09 12:32:44 +0000

    hosting

--- self_help/admin.py
+++ self_help/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserProfile, Conversation
 
-# Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Conversation)
