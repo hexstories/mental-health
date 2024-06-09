b20dd21de35cf0ff3c312c56a321f9dd7bb5986a
hexstories <kwakuasihene@gmail.com>
2024-06-09 10:33:23 +0000

    red

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
