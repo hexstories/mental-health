cfa59c58f79961e25a8e6b8b3e7d1a1f46b1995b
miky-rola <mikyrola8@gmail.com>
2024-08-11 21:59:13 +0000

    updated settings.py

--- core/settings.py
+++ core/settings.py
@@ -78,7 +78,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR /'templates')],
+        "DIRS": [str(APPS_DIR / "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
