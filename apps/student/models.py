2d6da1c99afcfd9714a6941191fcb7c885eb9135
hexstories <kwakuasihene@gmail.com>
2024-08-29 09:16:02 +0000

    test

--- apps/student/models.py
+++ apps/student/models.py
@@ -17,16 +17,19 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
-
-        return self.create_user(email, password, **extra_fields)
+    def create_superuser(self, email, username=None, password=None, **extra_fields):
+        """
+        Create and save a superuser with the given email and password.
+        """
+        if not username:
+            username = email.split("@")[0][:15]
+        user = self.create_user(
+            email, username=username, password=password, **extra_fields
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
   
 class User(AbstractBaseUser, PermissionsMixin, models.Model):
     email = models.EmailField(
--- core/settings.py
+++ core/settings.py
@@ -132,6 +132,13 @@ REST_FRAMEWORK = {
 ]
 }
 
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
