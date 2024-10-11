dae1a881359ed5020dce7a4c43a5a6b31e960abb
hexstories <kwakuasihene@gmail.com>
2024-10-11 08:56:35 +0000

    tests

--- core/settings.py
+++ core/settings.py
@@ -112,11 +112,7 @@ DATABASES = {
     }
 }
 
-# DATABASES = {"default": env.db("DATABASE_URL", default="sqlite:///db.sqlite3")}
-# DATABASES = env.db("DATABASE_URL", default="")  # noqa F405
-# DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
-# DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
-# Static files (CSS, JavaScript, Images)
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
